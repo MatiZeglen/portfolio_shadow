e4cbfcf5ba8286fe132e688927f7099a22612017
MatiZeglen <matizeglen2003@gmail.com>
2024-03-21 11:48:58 +0100

    WiP

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-const preview = document.getElementById('preview'), styles = document.getElementById('styles'), ranges = document.querySelectorAll('.settings input')
+const preview = document.getElementById('preview'), styles = document.getElementById('styles'), ranges = document.querySelectorAll('.settings input'), copyButton = document.getElementById('copy-styles');
+
+ranges.forEach((slider)=>
+{
+    slider.addEventListener("input", generateStyles);
+});
+
+function generateStyles()
+{
+    const xShadow = document.getElementById('x-shadow').value;
+    const yShadow = document.getElementById('y-shadow').value;
+    const blurRadius = document.getElementById('blur-r').value;
+    const spreadRadius = document.getElementById('spread-r').value;
+    const shadowColor = document.getElementById('shadow-color').value;
+    const shadowOpacity = document.getElementById('shadow-opacity').value;
+    const shadowInset = document.getElementById('shadow-inset').ariaChecked;
+    const borderRadius = document.getElementById('border-r').value;
+    
+    const boxShadow = `${shadowInset} ? "inset " : ""}${xShadow}px ${yShadow}px ${blurRadius}px ${spreadRadius}px ${hexToRgba(shadowColor, shadowOpacity)}`;
+}
